b50844d9b525ffa84df5c0a424632d1aab9ffb56
Alejandro Vidal <avd.formacion@gmail.com>
2022-11-20 13:54:40 +0100

    complicando ej09: versión panorámica

--- app/src/main/java/com/example/composesinnavigation/Ej09Activity.kt
+++ app/src/main/java/com/example/composesinnavigation/Ej09Activity.kt
@@ -4,8 +4,13 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -22,17 +27,36 @@ class Ej09Activity : ComponentActivity() {
 }
 
 
-@Preview (showSystemUi = true)
+@Preview(showSystemUi = true)
 @Composable
 private fun ContenidoEj09() {
     val configuration = LocalConfiguration.current
     when (configuration.orientation) {
         Configuration.ORIENTATION_LANDSCAPE -> {
-            Text("Landscape")
+            Row() {
+                Text("Landscape", Modifier.weight(1f))
+                CajaConCosas(Modifier.weight(2f).fillMaxSize())
+            }
         }
         else -> {
-            Text("Portrait")
+            Column() {
+                Text("Portrait", Modifier.weight(1f))
+                CajaConCosas(Modifier.weight(2f).fillMaxWidth())
+            }
         }
+    }
+}
+
+
+@Preview (widthDp = 150, heightDp = 150)
+@Composable
+private fun CajaConCosas(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier.background(Color.LightGray)) {
+        Text(text = "Texto en TopStart", Modifier.align(Alignment.TopStart).background(Color.Cyan))
+        Text(text = "Texto en BottomEnd", Modifier.align(Alignment.BottomEnd).background(Color.Green))
+
     }
 }
 
